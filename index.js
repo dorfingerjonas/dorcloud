5c51a963ad7fcb8bef5462d6fd63636fdc777f2a
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-03-25 10:24:32 +0100

    implement me and dorfi endpoints

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+// config
+const config = {
+    port: 80
+};
+
+// start express server on port
+http.listen(config.port, () => {
+    console.log(`http://localhost:${config.port}/`);
+});
+
+app.get('/dorfi', (req, res) => {
+    res.contentType('image/jpg');
+    res.sendFile(`${__dirname}\\assets\\images\\portrait.jpg`);
+});
+
+app.get('/me', (req, res) => {
+    res.contentType('image/jpg');
+    res.sendFile(`${__dirname}\\assets\\images\\portrait-2.jpg`);
+});
+
+app.get('/', (req, res) => {
+    res.contentType('application/json');
+    res.send('API Server is running');
+});
